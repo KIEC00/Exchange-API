39438c82f7cad4f7af9b0c263c3e96dd5840481c
Kiec00 <user.nullptr@gmail.com>
2025-05-15 20:43:42 +0500

    Add db session maker

--- app/core/__init__.py
+++ app/core/__init__.py
@@ -1,3 +1,4 @@
 from .config import config
+from .db import db
 
-__all__ = ("config",)
+__all__ = ("config", "db")
---
+++ app/core/db.py
@@ -0,0 +1,49 @@
+from typing import AsyncGenerator
+from sqlalchemy.ext.asyncio import (
+    AsyncEngine,
+    AsyncSession,
+    create_async_engine,
+    async_sessionmaker,
+)
+
+from app.core import config
+
+
+class DataBase:
+    def __init__(
+        self,
+        url: str,
+        echo: bool = False,
+        echo_pool=False,
+        pool_size=5,
+        max_overflow=10,
+    ):
+        self._engine: AsyncEngine = create_async_engine(
+            url=url,
+            echo=echo,
+            echo_pool=echo_pool,
+            pool_size=pool_size,
+            max_overflow=max_overflow,
+        )
+        self._session_maker: async_sessionmaker[AsyncSession] = async_sessionmaker(
+            bind=self._engine,
+            autoflush=False,
+            autocommit=False,
+            expire_on_commit=False,
+        )
+
+    async def dispose(self):
+        await self._engine.dispose()
+
+    async def session_maker(self) -> AsyncGenerator[AsyncSession, None]:
+        async with self._session_maker() as session:
+            yield session
+
+
+db = DataBase(
+    url=config.db.dsn,
+    echo=config.db.echo,
+    echo_pool=config.db.echo_pool,
+    pool_size=config.db.pool_size,
+    max_overflow=config.db.max_overflow,
+)
